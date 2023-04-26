459feac3d7c1b7342ca358f47d150f87a5dba616
Esteban <nameless.voidd@gmail.com>
2023-04-26 08:59:29 -0300

    Prueba referencia a imagenes para que se vean en vercel

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import"./Footer.css"
 
 const Footer = () =>{
-    return <footer className="footer" style={{backgroundImage:"url(./img/footer.png)"}}>
+    return <footer className="footer" style={{backgroundImage:"url(/img/footer.png)"}}>
         <div className="redes">
             <a href="https://www.instagram.com/esteban_candioti/">
-                <img src="./img/instagram.png" alt="Instagram" />
+                <img src="/img/instagram.png" alt="Instagram" />
             </a>
             <a href="https://github.com/EstebanCandioti">
-                <img src="./img/github.png" alt="Github" />
+                <img src="/img/github.png" alt="Github" />
             </a>
         </div>
-        <img src="./img/Logo.png" alt="org" />
+        <img src="/img/Logo.png" alt="org" />
         <strong>Desarrollado por Esteban Orfeo Candioti</strong>
     </footer>
 }
