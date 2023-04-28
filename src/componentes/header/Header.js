1b04c2374af2d4081dadc4a33f7ca98e76c3a5c2
Esteban <nameless.voidd@gmail.com>
2023-04-28 04:46:32 -0300

    pruba imagenes

--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css"
 
 function Header (){
     return <header className="header">    
-        <img src="/img/header.png" alt="org"/>
+        <img src="/img/Header.png" alt="org"/>
         </header>
 
 }
